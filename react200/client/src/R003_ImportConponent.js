1c4a997df10b1efb0bdbc92ea26d6d26481a3d48
clear200 <sangyeong.lee353@gmail.com>
2021-02-19 23:52:33 +0900

    React200

---
+++ react200/client/src/R003_ImportConponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class R003_ImportConponent extends Component {
+    render(){
+        return (
+            <h2>[THIS IS TIMPORTED COMPONENT]</h2>
+        )
+    }
+}
+
+export default R003_ImportConponent;   
