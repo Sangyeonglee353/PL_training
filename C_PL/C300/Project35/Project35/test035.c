579ceb70aa9481e28ec2af2d1a44c81bc1f9ba99
clear200 <sangyeong.lee353@gmail.com>
2021-02-28 19:47:57 +0900

    Project35

---
+++ C_PL/C300/Project35/Project35/test035.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main()
+{
+	int i = 1;
+	int hap = 0;
+
+	while (i <= 10) // i가 10보다 작거나 같은 동안
+	{
+		hap = hap + i;
+		i++; // i의 값을 1증가
+	}
+
+	printf("hap= %d", hap); // hap = 55
+}
