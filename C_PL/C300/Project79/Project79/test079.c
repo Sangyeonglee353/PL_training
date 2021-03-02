6b74c92703473895e6d44db60363ee55afa25944
clear200 <sangyeong.lee353@gmail.com>
2021-03-02 16:22:58 +0900

    Project79

---
+++ C_PL/C300/Project79/Project79/test079.c
@@ -0,0 +1,19 @@
+ // 문자열을 정수로 변환하기 3(strtol)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+	char* string = "0xFF";
+	char* stop;
+	int radix;
+	long value;
+
+	radix = 16;
+
+	value = strtol(string, &stop, radix);
+
+	printf("%d 개의 문자가 변환되었습니다. \n", stop-string);
+	printf("16진수 %s를 숫자로 변환하면 %ld입니다.\n", string, value);
+}
