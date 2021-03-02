d436adb6b4a7bd56ada9e77357aad4f6358d52e6
clear200 <sangyeong.lee353@gmail.com>
2021-03-02 21:36:50 +0900

    Project91

---
+++ C_PL/C300/Project91/Project91/test091.c
@@ -0,0 +1,23 @@
+// 문자가 알파벳 또는 숫자인지 검사하기(isalnum)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+	char* string = "Cat 1 Car 2 Cow 3....";
+	char buffer[100] = { 0, };
+	int cnt = 0;
+
+	while (*string)
+	{
+		if (isalnum(*string))
+		{
+			buffer[cnt++] = *string;
+		}
+
+		string++;
+	}
+
+	puts(buffer);
+}
