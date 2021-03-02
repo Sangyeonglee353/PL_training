c6d790c669fcd052514321a5ec09d90412c51e9d
clear200 <sangyeong.lee353@gmail.com>
2021-03-02 22:44:03 +0900

    Project98

---
+++ C_PL/C300/Project98/Project98/test098.c
@@ -0,0 +1,31 @@
+// 메모리 해제하기(free)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+#define MEGA 1024 * 1024
+
+void main(void)
+{
+	int i;
+	char* pmem;
+
+	for (i = 0; i < 10; i++)
+	{
+		pmem = malloc(MEGA);
+
+		if (pmem == NULL)
+		{
+			puts("메모리를 할당할 수 없습니다.");
+		}
+		else
+		{
+			puts("메모리를 1MB를 할당하였습니다.");
+
+			free(pmem);
+
+			puts("메모리를 해제하였습니다.");
+		}
+	}
+}
