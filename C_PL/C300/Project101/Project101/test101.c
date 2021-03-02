834b8b38c0428421df06bec99529e9101922fb30
clear200 <sangyeong.lee353@gmail.com>
2021-03-02 23:12:41 +0900

    Project101

---
+++ C_PL/C300/Project101/Project101/test101.c
@@ -0,0 +1,28 @@
+// 메모리 비교하기(memcmp)
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+void main(void)
+{
+	char s1[100] = "123";
+	char s2[100] = "123";
+
+	strcpy(&s1[4], "abc");
+	strcpy(&s2[4], "efg");
+
+	if (strcmp(s1, s2) == 0)
+	{
+		puts("strcmp: 버퍼의 값이 일치합니다.");
+	}
+
+	if (memcmp(s1, s2, 7) == 0)
+	{
+		puts("memcmp: 버퍼의 값이 일치합니다.");
+	}
+	else
+	{
+		puts("memcmp: 버퍼의 값이 일치하지 않습니다.");
+	}
+}
