a274bd16043abdda291d4f7186cc84063a146f34
clear200 <sangyeong.lee353@gmail.com>
2021-03-03 18:58:58 +0900

    Project103

---
+++ C_PL/C300/Project103/Project103/test103.c
@@ -0,0 +1,17 @@
+// 메모리 채우기(memset)
+
+#include <stdio.h>
+#include <string.h>
+
+void main(void)
+{
+	char string[50] = "아름다운 우리나라 대한민국";
+
+	puts(string);
+
+	memset(string, (int)NULL, sizeof(string));
+
+	memset(string, '*', sizeof(string) - 1);
+
+	puts(string);
+}
