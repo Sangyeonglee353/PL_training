fe76a65b4ccbfe8a698523fc7493ccbbcba4e085
clear200 <sangyeong.lee353@gmail.com>
2021-03-04 23:19:40 +0900

    Project120

---
+++ C_PL/C300/Project120/Project120/test120.c
@@ -0,0 +1,27 @@
+// 구조체 포인터 사용하기
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+struct tagAddress
+{
+	char name[30]; // 이름
+	char phone[20]; // 전화
+	char address[100]; // 주소
+};
+
+void main(void)
+{
+	struct tagAddress ad;
+	struct tagAddress* pad;
+
+	pad = &ad;
+
+	strcpy((*pad).name, "홍길동");
+	strcpy((*pad).phone, "02-1234-5678");
+	strcpy((*pad).address, "서울시 양천구 목동아파트 13단지");
+
+	printf("이름: %s \n", pad->name);
+	printf("전화: %s \n", pad->phone);
+	printf("주소: %s \n", pad->address);
+}
