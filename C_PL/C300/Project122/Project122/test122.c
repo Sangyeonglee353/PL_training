bf50236cd3152abbec4a3c5c7ca59c0c2154be27
clear200 <sangyeong.lee353@gmail.com>
2021-03-06 22:15:46 +0900

    Project122

---
+++ C_PL/C300/Project122/Project122/test122.c
@@ -0,0 +1,31 @@
+// 구조체 배열 사용하기
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+struct tagAddress
+{
+	char name[30]; // 이름
+	char phone[20]; // 전화
+	char address[100]; // 주소
+};
+
+void main(void)
+{
+	struct tagAddress ad[3];
+	int i;
+
+	for (i = 0; i < 3; i++)
+	{
+		sprintf(ad[i].name, "홍길동 %d", i);
+		strcpy(ad[i].phone, "02-1234-5678");
+		strcpy(ad[i].address, "서울시 양천구 목동아파트 13단지");
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		printf("이름: %s \n", ad[i].name);
+		printf("전화: %s \n", ad[i].phone);
+		printf("주소: %s \n", ad[i].address);
+	}
+}
