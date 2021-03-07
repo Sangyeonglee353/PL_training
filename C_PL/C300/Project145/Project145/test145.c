869123321fba991c6dfffbcd451693b39286ad42
clear200 <sangyeong.lee353@gmail.com>
2021-03-07 22:55:49 +0900

    Project145

---
+++ C_PL/C300/Project145/Project145/test145.c
@@ -0,0 +1,24 @@
+// 파일 포인터를 처음으로 이동하기 1(fseek)
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main(void)
+{
+	FILE* fp;
+
+	fp = fopen("d:\\mysources\\file.txt", "w+");
+
+	if (fp == NULL)
+	{
+		puts("파일을 생성할 수 없습니다.");
+	}
+	else
+	{
+		printf("파일 포인터의 위치: %d \n", ftell(fp)); // 위치: 0
+		fputs("abcde", fp);
+		printf("파일 포인터의 위치: %d \n", ftell(fp)); // 위치: 5
+		fseek(fp, 0L, SEEK_SET);
+		printf("파일 포인터의 위치: %d \n", ftell(fp)); // 위치: 0
+		fclose(fp);
+	}
+}
