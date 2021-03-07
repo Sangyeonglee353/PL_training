9ec05f70ec130a44644bc091d5dd22451165088b
clear200 <sangyeong.lee353@gmail.com>
2021-03-07 21:48:02 +0900

    Project139

---
+++ C_PL/C300/Project139/Project139/test139.c
@@ -0,0 +1,20 @@
+// 파일에 문자열 쓰기(fputs)
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main(void)
+{
+	FILE* fp;
+
+	fp = fopen("d:\\mysources\\file.txt", "w+");
+
+	if (fp == NULL)
+	{
+		puts("파일을 생성할 수 없습니다.");
+	}
+	else
+	{
+		fputs("대한민국 \n", fp);
+		fclose(fp);
+	}
+}
