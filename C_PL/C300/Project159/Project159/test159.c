88f4142acbd1b8d491ef9b5ff89524d83dbd584b
clear200 <sangyeong.lee353@gmail.com>
2021-03-08 21:19:43 +0900

    Project159

---
+++ C_PL/C300/Project159/Project159/test159.c
@@ -0,0 +1,19 @@
+// 파일 속성 변경하기(_chmod)
+
+#include<stdio.h>
+#include<io.h>
+#include<sys/stat.h>
+
+void main(void)
+{
+	char* filename = "d:\\mysources\\file_1.txt";
+
+	if (_chmod(filename, _S_IREAD) != 0)
+	{
+		perror("파일 속성 설정 에러");
+	}
+	else
+	{
+		puts("파일의 속성을 성공적으로 설정하였습니다.");
+	}
+}
