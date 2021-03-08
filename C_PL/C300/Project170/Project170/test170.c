3b97881e34b765d141b461475256a408e3684e9c
clear200 <sangyeong.lee353@gmail.com>
2021-03-08 21:59:18 +0900

    Project170

---
+++ C_PL/C300/Project170/Project170/test170.c
@@ -0,0 +1,19 @@
+// 날짜 및 시간 구하기 2 (_ftime)
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<time.h>
+#include<sys/timeb.h>
+
+void main(void)
+{
+	struct _timeb tb;
+	struct tm t;
+
+	_ftime(&tb);
+
+	t = *localtime(&tb.time);
+
+	printf("현재 날짜 및 시간: %4d.%d.%d %d:%d:%d.%d \n",
+		t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
+		t.tm_hour, t.tm_min, t.tm_sec, tb.millitm);
+}
