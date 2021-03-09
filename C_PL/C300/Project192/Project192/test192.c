03b26f1cc41f75fe92ddfe76f1cc8f781d9a3b30
clear200 <sangyeong.lee353@gmail.com>
2021-03-09 23:46:00 +0900

    Project192

---
+++ C_PL/C300/Project192/Project192/test192.c
@@ -0,0 +1,14 @@
+// 매크로 상수 정의하기
+
+#include <stdio.h>
+
+#define program void main(void)
+#define println printf
+
+#define MAX 1000
+#define MIN 0
+
+program
+{
+	println("MAX: %d, MIN: %d \n", MAX, MIN);
+}
