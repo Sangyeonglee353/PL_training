1601542b8d31da02055ffe2333167e7b2481341c
clear200 <sangyeong.lee353@gmail.com>
2021-03-09 23:38:17 +0900

    Project190

---
+++ C_PL/C300/Project190/Project190/test190.c
@@ -0,0 +1,27 @@
+// 숫자 정렬하기(qsort)
+
+#include <Stdio.h>
+#include <stdlib.h>
+
+int intcmp(const void* v1, const void* v2);
+
+void main(void)
+{
+	int i;
+	int array[5] = { 5, 3, 1, 2, 4 };
+
+	qsort(array, 5, sizeof(array[0]), intcmp);
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d", array[i]);
+	}
+}
+
+int intcmp(const void* v1, const void* v2)
+{
+	int cmpvalue1 = *(int*)v1;
+	int cmpvalue2 = *(int*)v2;
+
+	return cmpvalue1 - cmpvalue2;
+}
