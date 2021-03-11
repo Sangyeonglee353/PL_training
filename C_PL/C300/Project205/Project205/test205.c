c9773085ba4def9aab85c36bb3afe9b3b32b5069
clear200 <sangyeong.lee353@gmail.com>
2021-03-11 22:30:43 +0900

    Project205

---
+++ C_PL/C300/Project205/Project205/test205.c
@@ -0,0 +1,18 @@
+// 1~100까지 홀수의 합 구하기
+
+#include <stdio.h>
+
+void main(void)
+{
+	int i, hap = 0;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 2)
+		{
+			hap += i;
+		}
+	}
+
+	printf("1~100까지의 홀수의 합: %d \n", hap);
+}
