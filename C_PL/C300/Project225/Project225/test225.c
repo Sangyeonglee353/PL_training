b2536e4888769f33e946140d7a1b8ec264d9f103
clear200 <sangyeong.lee353@gmail.com>
2021-03-18 22:29:45 +0900

    Project225

---
+++ C_PL/C300/Project225/Project225/test225.c
@@ -0,0 +1,16 @@
+// 문자열의 좌우 특정 문자들 제거하기(strspn, strcspn)
+
+#include <stdio.h>
+#include <string.h>
+
+void main(void)
+{
+	char string[] = "  .;;abc;;.  ";
+	char* sep = " .;";
+	int nIndex1, nIndex2;
+
+	nIndex1 = strspn(string, sep);
+	nIndex2 = strcspn(&string[nIndex1], sep);
+	(&string[nIndex1])[nIndex2] = 0;
+	printf("%s \n", &string[nIndex1]);
+}
