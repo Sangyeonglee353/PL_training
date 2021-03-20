e47c9a4d9595869e7efbf4599d2c949e880886ac
clear200 <sangyeong.lee353@gmail.com>
2021-03-20 22:31:43 +0900

    Project231

---
+++ C_PL/C300/Project231/Project231/test231.c
@@ -0,0 +1,27 @@
+// 메모리를 100MB 할당하고 배제하기
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+#define MEGA_BYTE 1048576
+
+void main(void)
+{
+	char* pbuf;
+
+	pbuf = malloc(100 * MEGA_BYTE);
+
+	if (pbuf)
+	{
+		memset(pbuf, 0, 100*MEGA_BYTE);
+		
+		strcpy(&pbuf[0], "서울시 양천구 목동");
+		puts(&pbuf[0]);
+
+		strcpy(&pbuf[104857500], "부산시 강서구 미음동");
+		puts(&pbuf[104857500]);
+
+		free(pbuf);
+	}
+}
