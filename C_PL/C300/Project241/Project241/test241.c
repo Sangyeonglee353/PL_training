b33107ae08855299125e3917847d50c7478e1468
clear200 <sangyeong.lee353@gmail.com>
2021-03-21 23:45:46 +0900

    Project241

---
+++ C_PL/C300/Project241/Project241/test241.c
@@ -0,0 +1,85 @@
+// 두 개의 파일 합치기
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define FILEREAD 4096
+
+void main(void)
+{
+	FILE* fpR1, * fpR2, * fpW;
+	char buff[FILEREAD];
+	int len;
+
+	fpR1 = fopen("D:\\mysources\\file_1.bin", "rb");
+
+	if (fpR1 == NULL)
+	{
+		perror("파일 읽기 개방 에러");
+		return;
+	}
+
+	fpR2 = fopen("D:\\mysources\\file_copy.bin", "rb");
+
+	if (fpR2 == NULL)
+	{
+		perror("파일 읽기 개방 에러");
+		_fcloseall();
+		return;
+	}
+
+	fpW = fopen("D\\mysources\\file2.bin", "w+b");
+
+	if (fpW == NULL)
+	{
+		perror("파일 쓰기 개방 에러");
+		_fcloseall();
+		return;
+	}
+
+	while (!feof(fpR1))
+	{
+		len = fread(buff, 1, FILEREAD, fpR1);
+		if (ferror(fpR1))
+		{
+			perror("파일 읽기 에러 1");
+			_fcloseall();
+			return;
+		}
+
+		if (len > 0)
+		{
+			fwrite(buff, 1, len, fpW);
+			if (ferror(fpW))
+			{
+				perror("파일 쓰기 에러 1");
+				_fcloseall();
+				return;
+			}
+		}
+	}
+	while (!feof(fpR2))
+	{
+		len = fread(buff, 1, FILEREAD, fpR2);
+		if (ferror(fpR2))
+		{
+			perror("파일 읽기 에러 2");
+			_fcloseall();
+			return;
+		}
+
+		if (len > 0)
+		{
+			fwrite(buff, 1, len, fpW);
+			if (ferror(fpW))
+			{
+				perror("파일 쓰기 에러 2");
+				_fcloseall();
+				return;
+			}
+		}
+	}
+
+	_fcloseall();
+
+	puts("파일이 성공적으로 합쳐졌습니다.");
+}
