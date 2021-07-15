736f5bd30f6cb1917f2f693ddd8e08438e0621b5
Patiencelee <Sangyeonglee353@gmail.com>
2021-07-15 17:07:06 +0900

    edit

--- Python_PL/Self_Study_Python/ch04_01_03.py
+++ Python_PL/Self_Study_Python/ch04_01_03.py
@@ -7,9 +7,4 @@ for number in numbers:
         print(number,"는 짝수입니다.")
 
 for number in numbers:
-    if int(number / 100) != 0:
-        print(number," 3 자릿수입니다.")
-    elif int(number / 10) != 0:
-        print(number,"는 2 자릿수입니다.")
-    else:
-        print(number,"는 1 자릿수입니다.")
+    print(number, "는", len(str(number)), "자릿수입니다.")
