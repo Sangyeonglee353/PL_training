9165caf09c6f342b98943426538c88950595dd86
PatienceLee <Sangyeong.lee353@gmail.com>
2021-07-31 13:59:00 +0900

    python ch06_01

---
+++ Python_PL/Self_Study_Python/ch06_01_02.py
@@ -0,0 +1,25 @@
+# 06-1 | 구문 오류와 예외 확인문제 2
+
+numbers = [52, 273, 32, 103, 90, 10, 275]
+
+print("# (1) 요소 내부에 있는 값 찾기")
+print("- {}는 {} 위치에 있습니다.".format(52, numbers.index(52)))
+print()
+
+print("# (2) 요소 내부에 없는 값 찾기_조건문")
+number = 10000
+if number in numbers:
+    print("- {}는 {} 위치에 있습니다.".format(number, numbers.index(number)))
+else:
+    print("- 리스트 내부에 없는 값입니다.")
+print()
+
+print("# (2) 요소 내부에 없는 값 찾기_try except")
+number = 10000
+try:
+    print("- {}는 {} 위치에 있습니다.".format(number, numbers.index(number)))
+except:
+    print("- 리스트 내부에 없는 값입니다.")
+print()
+
+print("--- 정상적으로 종료되었습니다. ---")
