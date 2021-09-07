3b3169ce4ad8e9f5dc387a87379a34374e04413b
PatienceLee <Sangyeong.lee353@gmail.com>
2021-09-07 20:06:41 +0900

    Java 2weeks_Chapter02

---
+++ Java_PL/Java_essential/Chapter02_02/src/Var.java
@@ -0,0 +1,12 @@
+// var 키워드를 사용하여 변수 선언
+public class Var {
+	public static void main(String[] args) {
+		var price = 200;  // price는 int 타입으로 결정
+		var name = "kitae"; // name은 String 타입으로 결정
+		var pi = 3.14; // pi는 double 타입으로 결정
+		
+		System.out.println("price = " + (price + 1000));
+		System.out.println("name = " + name);
+		System.out.println("pi = " + pi*10);
+	}
+}
