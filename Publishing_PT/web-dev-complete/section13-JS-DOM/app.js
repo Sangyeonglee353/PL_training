bf605ecf264bd70c8fb201916b493f490a3e2ff7
Sang Yeong Lee <46237147+Sangyeonglee353@users.noreply.github.com>
2022-12-31 23:25:11 +0900

    Upload section13-JS-DOM [DAY 33]

---
+++ Publishing_PT/web-dev-complete/section13-JS-DOM/app.js
@@ -0,0 +1,16 @@
+console.log(window.document);
+console.log(document);
+console.dir(document);
+/* window 객체의 메서드 사용법 */
+// 방법1
+// window.alert();
+
+// 방법 2
+// alert();
+
+// 방법 1_anchor 태그의 href 접근하기
+// console.log(document.body.children[1].children[0].href);
+
+// 새로운 값 할당
+// document.body.children[1].children[0].href = 'https://google.com';
+// console.log(document.body.children[1].children[0].href);
