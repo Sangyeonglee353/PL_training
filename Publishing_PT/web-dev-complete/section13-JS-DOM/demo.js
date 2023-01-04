915e132f1425e42600d4d536430a429c270021c4
Sang Yeong Lee <46237147+Sangyeonglee353@users.noreply.github.com>
2023-01-04 22:39:41 +0900

    Update section13-JS-DOM [DAY36]

---
+++ Publishing_PT/web-dev-complete/section13-JS-DOM/demo.js
@@ -0,0 +1,15 @@
+const productNameInputElement = document.getElementById('product-name');
+const remainingCharsElement = document.getElementById('remaining-chars');
+
+// maxLength, camel case 사용한다는 것 잊지말 것!
+const maxAllowedChars = productNameInputElement.maxLength;
+
+function updateRemainingCharacters(event) {
+    const enteredText = event.target.value;
+    const enteredTextLength = enteredText.length;
+    
+    const remainingCharacters = maxAllowedChars - enteredTextLength;
+
+    remainingCharsElement.textContent = remainingCharacters;
+}
+productNameInputElement.addEventListener('input', updateRemainingCharacters);
