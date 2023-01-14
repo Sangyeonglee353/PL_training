79e99d3a3a8c437615cbdc574d6a999b83c9a69b
Sang Yeong Lee <46237147+Sangyeonglee353@users.noreply.github.com>
2023-01-14 22:04:29 +0900

    section14-Tic-Tac-Toe-Game [DAY42] modal-data Management

--- Publishing_PT/web-dev-complete/section14-Tic-Tac-Toe-Game/scripts/app.js
+++ Publishing_PT/web-dev-complete/section14-Tic-Tac-Toe-Game/scripts/app.js
@@ -1,5 +1,21 @@
+let editedPlayer = 0; // 선택된 플레이어 번호
+
+// 플레이어 배열 저장하기
+const players = [
+    {
+        name: '',
+        symbol: 'X'
+    },
+    {
+        name: '',
+        symbol: 'O'
+    }
+]
+
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
+const errorsOutputElement = document.getElementById('config-errors');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
@@ -10,3 +26,5 @@ editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
+
+formElement.addEventListener('submit', savePlayerConfig);
--- Publishing_PT/web-dev-complete/section14-Tic-Tac-Toe-Game/scripts/config.js
+++ Publishing_PT/web-dev-complete/section14-Tic-Tac-Toe-Game/scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid; // +'1' => 1
     playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
 }
@@ -6,4 +7,36 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error'); // 양식 초기화
+    errorsOutputElement.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = ''; // 입력 요소 제거하기
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault(); // 기본 명령 제거
+    console.log(event);
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername').trim();
+    if(!enteredPlayername){ // enteredPlayername === ''
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContent = 'Please enter a valid name!';
+        return; // 함수 실행 멈추기
+    }
+
+    // 입력된 플레이어 이름 반영하기(동적)
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+    // 입력된 플레이어 이름 배열에 저장하기_방법 1
+    // if(editedPlayer === 1){
+    //     players[0].name = enteredPlayername;
+    // }else{
+    //     players[1].name = enteredPlayername;
+    // }
+
+    // 입력된 플레이어 이름 배열에 저장하기_방법 2
+    players[editedPlayer - 1].name = enteredPlayername;
+
+    // 종료하기
+    closePlayerConfig();
 }
