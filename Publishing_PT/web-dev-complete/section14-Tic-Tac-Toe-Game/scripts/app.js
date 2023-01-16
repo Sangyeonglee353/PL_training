ab4e88c1161052c44d48759a7a9f7a8b54aff180
Sang Yeong Lee <46237147+Sangyeonglee353@users.noreply.github.com>
2023-01-16 23:46:52 +0900

    Update section14-Tic-Tac-Toe-Game [DAY43] Game Round Turn

--- Publishing_PT/web-dev-complete/section14-Tic-Tac-Toe-Game/scripts/app.js
+++ Publishing_PT/web-dev-complete/section14-Tic-Tac-Toe-Game/scripts/app.js
@@ -1,4 +1,11 @@
+const gameData = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
 let editedPlayer = 0; // 선택된 플레이어 번호
+let activePlayer = 0;
 
 // 플레이어 배열 저장하기
 const players = [
@@ -16,10 +23,15 @@ const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
+const startNewGameBtnElement = document.getElementById('start-game-btn');
+// const gameFieldElements = document.querySelectorAll('#game-board li');
+const gameBoardElement = document.getElementById('game-board');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -28,3 +40,13 @@ cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit', savePlayerConfig);
+
+startNewGameBtnElement.addEventListener('click', startNewGame);
+
+// 방법1_ 배열의 반복 'for...of'
+// for (const gameFieldElement of gameFieldElements) {
+//     gameFieldElement.addEventListener('click', selectGameField);
+// }
+
+// 방법2
+gameBoardElement.addEventListener('click', selectGameField);
